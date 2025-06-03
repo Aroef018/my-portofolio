61896829c3df7cc3a6412e31745ce0e4606a7c15
Aroef <amarsaja42@gmail.com>
2025-06-04 05:35:25 +0700

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 // src/App.tsx
-import React from "react";
 import { BrowserRouter as Router } from "react-router-dom"; // Import Router
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
@@ -7,41 +6,12 @@ import AboutSection from "./components/AboutSection";
 import SkillsSection from "./components/SkillsSection";
 import ProjectsSection from "./components/ProjectsSection";
 import ExperienceSection from "./components/ExperienceSection";
-// import ContactSection from "./components/ContactSection"; // Ini masih dikomentari, biarkan saja
 import Footer from "./components/Footer";
 import AwardsSection from "./components/AwardsSection";
-
-// --- HAPUS SELURUH BLOK KOMPONEN SCROLLTOSECTION DARI SINI ---
-// const ScrollToSection: React.FC<{ children: React.ReactNode }> = ({
-//   children,
-// }) => {
-//   const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-//     e.preventDefault();
-//     const targetId = e.currentTarget.href.split("#")[1];
-//     const targetElement = document.getElementById(targetId);
-//     if (targetElement) {
-//       targetElement.scrollIntoView({ behavior: "smooth" });
-//     }
-//   };
-//
-//   return React.Children.map(children, (child) => {
-//     if (
-//       React.isValidElement(child) &&
-//       typeof child.type === "string" &&
-//       child.type === "a"
-//     ) {
-//       return React.cloneElement(child, { onClick: handleScroll });
-//     }
-//     return child;
-//   });
-// };
-// --- HINGGA SINI ---
 
 function App() {
   return (
     <Router>
-      {" "}
-      {/* Bungkus seluruh aplikasi dengan Router */}
       <div className="min-h-screen bg-dark-background text-text-light flex flex-col">
         <Header />
         <main className="flex-grow">
@@ -51,7 +21,6 @@ function App() {
           <SkillsSection />
           <ProjectsSection />
           <AwardsSection />
-          {/* <ContactSection /> */}
         </main>
         <Footer />
       </div>
